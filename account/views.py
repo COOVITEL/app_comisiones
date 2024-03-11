c43e57280a06a8a2e469e4bf3b6d985724077d34
COOVITEL <direcciontop@coovitel.coop>
2024-03-11 16:30:56 -0500

    add dashboard and login with styles

--- account/views.py
+++ account/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from users.models import Asesor, Sucursale
 
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    asesors = Asesor.objects.count()
+    sucursales = Sucursale.objects.count()
+    return render(request, 'account/dashboard.html',
+                  {'section': 'dashboard',
+                   'asesors': asesors,
+                   'sucursales': sucursales})
+
